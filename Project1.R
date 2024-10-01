ae09a40d4b164d0c9fba593631dfbbaed04d1c29
Jonas-student <jonas.meijerink@gmail.com>
2024-10-01 21:46:39 +0200

    Begin of the simulation model

--- Project1.R
+++ Project1.R
@@ -6,7 +6,26 @@
 
 ### Exposure assessment --------------------------------------------------------
 
-# test test
+# Prevalence of Campylobacter spp. in poultry carcasses at slaughterhouses
+Ps <- rbeta(10 + 1, 30 - 10 + 1)
+
+# Prevalence of Campylobacter spp. in poultry carcasses at retail
+Pr <- rbeta(25 + 1, 30 - 25 + 1)
+
+# Number of Campylobacter spp. in poultry meat at retail
+Cr <- 
+
+# Cross-contamination factor
+Fcc <- Pr/Ps
+
+# Probability of infected carcass 
+Pc <- Fcc * Ps / (1 - Ps + Fcc * Ps)
+
+# Uncertainty about the storage condition
+Sc <- rbinom(n, size, prob=)
+
+#
+Rr <- 
 
 
 ### Dose response --------------------------------------------------------------
@@ -17,4 +36,4 @@
 
 ### Risk characterization ------------------------------------------------------
 
-
+# 
