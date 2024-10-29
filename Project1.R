66b555890ea11cb78721ce65cd536e7a7169cf70
Jonas-student <jonas.meijerink@gmail.com>
2024-10-29 16:25:20 +0100

    Updates regarding the results

--- Project1.R
+++ Project1.R
@@ -8,11 +8,12 @@
 set.seed(485721023)
 
 
-simulation <- function(N , FreezeOnly){
+simulation <- function(N , Storage){
   ## This function requires the following input parameters:
   # - N : the total number of simulations
-  # - FreezeOnly : 1 storage condition always freezing 
-  #                0 storage condition has two options freeze/fresh
+  # - Storage : 2 storage condition always fresh
+  #             1 storage condition always freezing 
+  #             0 storage condition has two options freeze/fresh
 
   ### Define all variables -----------------------------------------------------
   Ps <- rep(NA,N)       # Prevalence in poultry carcasses at slaughterhouses
@@ -72,10 +73,13 @@ simulation <- function(N , FreezeOnly){
     
     
     ## Uncertainty about the storage condition
-    if (FreezeOnly==0){
+    if (Storage==0){
     Sc[i] <- rbinom(n = 1 , size = 1, prob = runif(1 , min = 0 , max = 1))
-    }else{
+    }
+    if(Storage==1){
       Sc[i] <- 0
+    }else{
+      Sc[i] <- 1 
     }
     # Refrigeration = 1
     # Freeze = 0
@@ -152,31 +156,34 @@ simulation <- function(N , FreezeOnly){
 ## Load libraries
 library(ggplot2)
 library(gridExtra)
+library(ltm)
+library(grid)
 
 
 ## Output from the simulation model
-out <- data.frame(simulation(N = 100000 , FreezeOnly = 0))
+out <- data.frame(simulation(N = 100000 , Storage = 0))
 
 
-## 3.1 What is the amount of Campylobacter spp. to which a consumer is exposed 
+## 3.1 What is the amount of Campylobacter spp. to which a consumer is exposed
 ## in a single serving of poultry meat? !!--> change y-axis!!
 
 # Visualisations
-plot1 <- ggplot(aes(x=X5) , data=out) + geom_histogram(aes(y = ..density..),fill="#69b3a2",
-  binwidth = 1 , alpha=1 ) + 
-  labs(y = "Density", x = "MPN per serving") + theme_minimal() + 
+plot1 <- ggplot(aes(x=X5) , data=out) + geom_histogram(aes(y = ..density..),
+                            fill="#69b3a2",binwidth = 1 , alpha=1 ) +
+  labs(y = "Density", x = "MPN per serving") + theme_minimal() +
   ylim(0,0.6)
 
 plot2 <- ggplot(aes(x=X5) , data=out) + geom_histogram(aes(y = ..density..),
-  fill="#69b3a2", color="#e9ecef", binwidth = 1  ) + 
-  labs(y = "Density", x = "MPN per serving") + theme_minimal() + xlim(-1,80)+
-  geom_vline(xintercept=10, color="gray", linetype="dashed", size=0.7) + 
+                                                       fill="#69b3a2", color="#e9ecef", binwidth = 1.07) +
+  labs(y = "Density", x = "MPN per serving") + theme_minimal() + xlim(-1,48)+
+  geom_vline(xintercept=10, color="gray1", linetype="dashed", size=0.75) +
   ylim(0,0.6)
 
-grid.arrange(plot1 , plot2 , ncol=2 , top = "Number of Campylobacter spp. per serving")
+title <- textGrob("Number of Campylobacter spp. per serving", gp=gpar(fontsize=18))
+grid.arrange(plot1, plot2, ncol=2, top=title)
 
 
-# Summary statistics !!add aditional one (HPD)!!
+# Summary statistics 
 sum(out$X5<49)/100000
 median(out$X5)
 
@@ -192,27 +199,60 @@ sum(out$X5>0)/100000
 ## 3.4 What is the probability of illness for a single meal of chicken?
 mean(out$X6)
 
-ggplot(aes(x=X6) , data=out) +  geom_histogram(fill="#69b3a2", color="#e9ecef",
-alpha=0.8 , binwidth = 0.01) +
-  ggtitle("Probability of illness per serving") + labs(y = "Density", x = "Probability of illnes")+
-  theme_minimal() + xlim(-0.01,1.01)
+ggplot(aes(x=X6) , data=out) +  
+  geom_histogram(aes(y = ..density../100),fill="#69b3a2", color="#e9ecef",
+                                               alpha=0.8 , binwidth = 0.01) +
+  ggtitle("Probability of illness per serving") + 
+  labs(y = "Density", x = "Probability of illnes")+
+  theme_minimal() + xlim(-0.01,0.45) + 
+  theme(plot.title = element_text(size = 20, hjust = 0.5))
 
 
-## 3.5 Estimate the number of people who could suffer from campylobacteriosis 
+## 3.5 Estimate the number of people who could suffer from campylobacteriosis
 ## related to poultry meat consumption.
 mean(out$X7)
 
 
 ## 3.6 Investigate using Spearman’s rank correlation coefficients which factors 
 ## in the chain have the largest impact on the risk of illness.
-cor(out , method = "spearman")
+
+correlation <- data.frame(coef = cor(out , method = "spearman")[1:5,6])
+correlation$factores <- c("Prevalence in poultry carcasses at slaughterhouses", 
+  "Prevalence in poultry carcasses at retail", "Storage condition" , 
+  "Cooking reduction  " , "Number (MPN) of Campylobacter per serving")
+
+correlation[3,1] <- biserial.cor(x=out$X6, y=out$X3, use = c("all.obs"),level=2)
+
+ggplot(data=correlation , aes(x=coef , y=factores , fill=c("1","1","1","0","1"))) + 
+  geom_bar(stat = "identity") +xlim(-0.01,1) + geom_col() +
+  geom_text(aes(label = round(coef,5)), hjust = -0.1, size = 3) + 
+  ggtitle("Correlation with factors that impact the risk of illness")+  theme_minimal() + 
+  theme(legend.position="none" , plot.title = element_text(hjust = -0.8)) + 
+  labs(x="Correlation*" , y="")
 
 
 ## 3.7 Investigate the impact of storage: assume that 100% of chickens were stored frozen, what
 ## would be the impact on the human campoylobacteriosis risk as compared to if none of
 ## the chicken were stored frozen?
-out_freeze_only <- data.frame(simulation(N = 100000 , FreezeOnly = 1))
 
+out_freeze_only <- data.frame(simulation(N = 100000 , Storage = 1))
+out_fridg_only <- data.frame(simulation(N = 100000 , Storage = 2))
 
+a<-hist(x=out_fridg_only $X6)
+b<-hist(x=out_freeze_only$X6)
+a<-data.frame("mids"=a$mids , "counts"=a$counts ,"Fmids"  = c(b$mids,0.39,0.41,0.43,0.45) , "Fcounts" = c(b$counts,1,1,1,1))
+a_long <- pivot_longer(a, 
+                       cols = everything(),
+                       names_to = c("type", ".value"),
+                       names_pattern = "(F?)(mids|counts)")
 
+ggplot(a_long, aes(x = as.factor(mids), y = counts, fill = type)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  scale_y_log10(breaks = c(10^1, 10^3, 10^5), labels = c(1, 3, 5)) +  # Custom breaks and labels for y-axis
+  labs(y = "Frequency on the log10 scale", 
+       x = "Probability of illness",
+       fill="Storage condition") +
+  scale_fill_discrete(labels = c("Refrigerator", "Freezer"))+
+  scale_x_discrete(breaks = c("0.05", "0.15", "0.25", "0.35", "0.45")) +  # Set specific x-axis labels
+  theme_minimal()
 
